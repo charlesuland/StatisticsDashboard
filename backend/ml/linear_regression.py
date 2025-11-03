b2586656814bf1471ef5e4a824c0e32958d00d44
Beaverr <squeaky.beaver4133@protonmail.com>
2025-11-02 18:01:18 -0600

    Clean up imports, don't divide test split by 1000, begin system for returning only useful measurements of models

--- backend/ml/linear_regression.py
+++ backend/ml/linear_regression.py
@@ -1,24 +1,24 @@
 import pandas as pd
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 
 class LinRegManager:
     def __init__(self, dataframe: pd.DataFrame, test_split):
         self.df = dataframe
         self.test_split = test_split / 100
-    
-    def train_linear_regression(self, target, features):
-        X = self.df[features]
-        y = self.df[target]
+
+    # renamed to `train` to match abstract interface and use self.test_split
+    def train(self, target, features):
+        X = self.df[features].copy()
+        y = self.df[target].copy()
 
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=self.test_split/100, random_state=42
+            X, y, test_size=self.test_split, random_state=42
         )
 
         model = LinearRegression()
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
 
-        return {"r2_score": r2_score(y_test, y_pred)}
+        return {"r2_score": r2_score(y_test, y_pred), "mse": mean_squared_error(y_test, y_pred)}
--- backend/ml/logistic_regression.py
+++ backend/ml/logistic_regression.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from sklearn.metrics import r2_score
+from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 
 class LogRegManager:
     def __init__(self, dataframe: pd.DataFrame, test_split):
@@ -14,11 +13,11 @@ class LogRegManager:
         y = self.df[target].copy()
 
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=self.test_split/100, random_state=42
+            X, y, test_size=self.test_split, random_state=42
         )
 
-        model = LogisticRegression()
+        model = LogisticRegression(max_iter=1000)
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
 
-        return {"r2_score": r2_score(y_test, y_pred)}
+        return {"accuracy": accuracy_score(y_test, y_pred)}
